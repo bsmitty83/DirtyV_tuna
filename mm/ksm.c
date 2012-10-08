9da5721b042b94cef43c35864db659f2b968f5f0
Konstantin Khlebnikov <khlebnikov@openvz.org>
2012-10-08 16:28:37 -0700

    mm: introduce arch-specific vma flag VM_ARCH_1
    Combine several arch-specific vma flags into one.
    
    before patch:
    
            0x00000200      0x01000000      0x20000000      0x40000000
    x86     VM_NOHUGEPAGE   VM_HUGEPAGE     -               VM_PAT
    powerpc -               -               VM_SAO          -
    parisc  VM_GROWSUP      -               -               -
    ia64    VM_GROWSUP      -               -               -
    nommu   -               VM_MAPPED_COPY  -               -
    others  -               -               -               -
    
    after patch:
    
            0x00000200      0x01000000      0x20000000      0x40000000
    x86     -               VM_PAT          VM_HUGEPAGE     VM_NOHUGEPAGE
    powerpc -               VM_SAO          -               -
    parisc  -               VM_GROWSUP      -               -
    ia64    -               VM_GROWSUP      -               -
    nommu   -               VM_MAPPED_COPY  -               -
    others  -               VM_ARCH_1       -               -
    
    And voila! One completely free bit.
    
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@openvz.org>
    Cc: Alexander Viro <viro@zeniv.linux.org.uk>
    Cc: Carsten Otte <cotte@de.ibm.com>
    Cc: Chris Metcalf <cmetcalf@tilera.com>
    Cc: Cyrill Gorcunov <gorcunov@openvz.org>
    Cc: Eric Paris <eparis@redhat.com>
    Cc: H. Peter Anvin <hpa@zytor.com>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: James Morris <james.l.morris@oracle.com>
    Cc: Jason Baron <jbaron@redhat.com>
    Cc: Kentaro Takeda <takedakn@nttdata.co.jp>
    Cc: Matt Helsley <matthltc@us.ibm.com>
    Cc: Nick Piggin <npiggin@kernel.dk>
    Cc: Oleg Nesterov <oleg@redhat.com>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Robert Richter <robert.richter@amd.com>
    Cc: Suresh Siddha <suresh.b.siddha@intel.com>
    Cc: Tetsuo Handa <penguin-kernel@I-love.SAKURA.ne.jp>
    Cc: Venkatesh Pallipadi <venki@google.com>
    Acked-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Conflicts:
    	include/linux/mm.h
    	mm/huge_memory.c
    	mm/ksm.c

--- mm/ksm.c
+++ mm/ksm.c
@@ -1496,9 +1496,9 @@ int ksm_madvise(struct vm_area_struct *vma, unsigned long start,
 		/*
 		 * Be somewhat over-protective for now!
 		 */
-		if (*vm_flags & (VM_MERGEABLE | VM_SHARED  | VM_MAYSHARE   |
-				 VM_PFNMAP    | VM_IO      | VM_DONTEXPAND |
-				 VM_RESERVED  | VM_HUGETLB |
+		if (*vm_flags & (VM_MERGEABLE | VM_SHARED | VM_MAYSHARE |
+				VM_PFNMAP | VM_IO | VM_DONTEXPAND |
+				VM_RESERVED | VM_HUGETLB | 
 				 VM_NONLINEAR | VM_MIXEDMAP))
 			return 0;		/* just ignore the advice */
 
