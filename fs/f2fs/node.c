0be00021631fbc20bde7c44c63f5c93ea5e08484
Brian_Smith <b671983@gmail.com>
2014-09-09 23:53:20 -0400

    Revert "f2fs: clean up an unused parameter and assignment"
    This reverts commit 95ad1434056acdafba80940aa30c4df3badb1f65.

--- fs/f2fs/node.c
+++ fs/f2fs/node.c
@@ -842,7 +842,7 @@ void remove_inode_page(struct inode *inode)
 	truncate_node(&dn);
 }
 
-struct page *new_inode_page(struct inode *inode)
+struct page *new_inode_page(struct inode *inode, const struct qstr *name)
 {
 	struct dnode_of_data dn;
 
