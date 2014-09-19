7222557f7f9676d63f49339cf233b96c550f1101
Brian_Smith <b671983@gmail.com>
2014-09-19 09:55:04 -0400

    fix last commit

--- fs/block_dev.c
+++ fs/block_dev.c
@@ -95,24 +95,6 @@ static void kill_bdev(struct block_device *bdev)
 }	
 EXPORT_SYMBOL(kill_bdev);
 
-/* Invalidate clean unused buffers and pagecache. */
-void invalidate_bdev(struct block_device *bdev)
-{
-	struct address_space *mapping = bdev->bd_inode->i_mapping;
-
-	if (mapping->nrpages == 0)
-		return;
-
-	invalidate_bh_lrus();
-	lru_add_drain_all();	/* make sure all lru add caches are flushed */
-	invalidate_mapping_pages(mapping, 0, -1);
-	/* 99% of the time, we don't need to flush the cleancache on the bdev.
-	 * But, for the strange corners, lets be cautious
-	 */
-	cleancache_invalidate_inode(mapping);
-}
-EXPORT_SYMBOL(invalidate_bdev);
-
 int set_blocksize(struct block_device *bdev, int size)
 {
 	/* Size must be a power of two, and between 512 and PAGE_SIZE */
