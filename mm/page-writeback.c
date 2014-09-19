d3eb86788cd03a6c94129d9cb9580ea701492eed
Brian_Smith <b671983@gmail.com>
2014-09-19 00:00:59 -0400

    fix no storage class error

--- mm/page-writeback.c
+++ mm/page-writeback.c
@@ -99,8 +99,8 @@ suspend_dirty_writeback_interval = 2000;
  */
 #define DEFAULT_DIRTY_EXPIRE_INTERVAL 200 /* centiseconds */
 unsigned int dirty_expire_interval,
-		resume_dirty_expire_interval;
-suspend_dirty_expire_interval = 1000;
+		resume_dirty_expire_interval,
+		suspend_dirty_expire_interval = 1000;
 
 /*
  * Flag that makes the machine dump writes/reads and block dirtyings.
